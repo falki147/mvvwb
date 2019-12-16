3559e770dde0efd479f5139651cda5e0ad41f6cf
Florian Preinfalk <preinfalkflorian@gmail.com>
2019-12-16 23:44:05 +0100

    Fix error when menu doesn't exist

--- src/MVVWB/Controller/HomeController.php
+++ src/MVVWB/Controller/HomeController.php
@@ -15,19 +15,21 @@ class HomeController {
         $menuObject = wp_get_nav_menu_object($locations['home-menu']);
         $menu = [];
 
-        foreach (wp_get_nav_menu_items($menuObject) as $menuItem) {
-            $item = [
-                'title' => $menuItem->title,
-                'url'   => $menuItem->url,
-                'text'  => ''
-            ];
+        if ($menuObject) {
+            foreach (wp_get_nav_menu_items($menuObject) as $menuItem) {
+                $item = [
+                    'title' => $menuItem->title,
+                    'url'   => $menuItem->url,
+                    'text'  => ''
+                ];
 
-            if ($menuItem->object === 'category')
-                $item['text'] = category_description($menuItem->object_id);
-            elseif ($menuItem->object !== 'custom')
-                $item['text'] = get_the_excerpt($menuItem->object_id);
+                if ($menuItem->object === 'category')
+                    $item['text'] = category_description($menuItem->object_id);
+                elseif ($menuItem->object !== 'custom')
+                    $item['text'] = get_the_excerpt($menuItem->object_id);
 
-            $menu[] = $item;
+                $menu[] = $item;
+            }
         }
 
         $name = get_bloginfo('name');
