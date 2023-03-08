cb91dab6538483709459604b5335aabac968eb32
Florian Preinfalk <preinfalkflorian@gmail.com>
2023-03-08 16:09:14 +0100

    Update year

--- src/MVVWB/Views/FooterView.php
+++ src/MVVWB/Views/FooterView.php
@@ -13,6 +13,6 @@ namespace MVVWB\Views;
         <?=$navigation?>
     </nav>
     <div class="copyright">
-        &copy; <a href="https://www.mvvwb.at/">Musikverein Vorderweissenbach</a>, 2020
+        &copy; <a href="https://www.mvvwb.at/">Musikverein Vorderweissenbach</a>, 2023
     </div>
 </footer>
